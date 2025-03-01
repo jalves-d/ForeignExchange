4bc295d22804a19a68a10d2c599b148a5df33022
João Neto <jalves-d@student.42lisboa.com>
2025-03-01 23:22:03 +0000

    Fix tests build, registration process tests and login need an update, change registration endpoint to return an authentication token

--- ForeignExchange.Tests/Services/UserServiceTests.cs
+++ ForeignExchange.Tests/Services/UserServiceTests.cs
@@ -2,6 +2,7 @@
 using ForeignExchange.Application.Interfaces;
 using ForeignExchange.Application.Services;
 using ForeignExchange.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Moq;
 
 namespace ForeignExchange.Tests.Services
@@ -11,13 +12,15 @@ namespace ForeignExchange.Tests.Services
         private readonly UserService _userService;
         private readonly Mock<IUserRepository> _userRepositoryMock;
         private readonly Mock<IPasswordHasherService> _passwordHasherServiceMock;
+        private readonly Mock<IHttpContextAccessor> _httpContextAcessor;
 
         public UserServiceTests()
         {
             _userRepositoryMock = new Mock<IUserRepository>();
 
             _passwordHasherServiceMock = new Mock<IPasswordHasherService>();
-            _userService = new UserService(_userRepositoryMock.Object, _passwordHasherServiceMock.Object);
+            _httpContextAcessor = new Mock<IHttpContextAccessor>();
+            _userService = new UserService(_userRepositoryMock.Object, _passwordHasherServiceMock.Object, _httpContextAcessor.Object);
         }
 
         [Fact]
@@ -29,7 +32,9 @@ namespace ForeignExchange.Tests.Services
 
             // Act
             var user = new UserDTO { Username = "testuser", Password = "password123" };
-            var result = await _userService.RegisterUserAsync(user);
+            var result = true;
+            try{ await _userService.RegisterUserAsync(user); }
+            catch (Exception) { result = false; };
 
             // Assert
             Assert.True(result);
@@ -41,11 +46,13 @@ namespace ForeignExchange.Tests.Services
         {
             // Arrange
             var userDto = new Domain.Entities.User { Username = "testuser", PasswordHash = "password123" };
-            _userRepositoryMock.Setup(repo => repo.RegisterUserAsync(userDto)).ReturnsAsync(false);
+            _userRepositoryMock.Setup(repo => repo.RegisterUserAsync(userDto));
 
             var user = new UserDTO { Username = "testuser", Password = "password123" };
             // Act
-            var result = await _userService.RegisterUserAsync(user);
+            var result = true;
+            try { await _userService.RegisterUserAsync(user); }
+            catch (Exception) { result = false; };
 
             // Assert
             Assert.False(result);
